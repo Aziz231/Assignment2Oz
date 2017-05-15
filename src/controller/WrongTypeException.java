66ddf864c472271b75000bc709885b96138297dd
Aziz231 <s3501099@student.rmit.edu.au>
2017-05-15 14:40:25 +1000

    WrongTypeException

--- src/controller/WrongTypeException.java
+++ src/controller/WrongTypeException.java
@@ -1,5 +1,13 @@
 package controller;
 
-public class WrongTypeException {
-
+public class WrongTypeException extends Exception {
+	String message;
+    public WrongTypeException(String message) {
+        this.message = message;
+    }
+    @Override
+    public String getMessage(){
+    	return message;
+    }
 }
+
