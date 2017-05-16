6ced5b0f4b545c09a40bf31d7910b0416aa5432c
turki <s3512039@student.rmit.edu.au>
2017-05-17 00:08:29 +1000

    sqlliteConnection

--- src/controller/SQLiteConnection.java
+++ src/controller/SQLiteConnection.java
@@ -1,5 +1,27 @@
 package controller;
 
-public class SQLiteConnection {
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class SQLiteConnection {
+	public static Connection connect(){
+		File file = new File("participants.sqlite");
+		try {
+			if(file.exists()){
+			Class.forName("org.sqlite.JDBC");	
+			Connection conn;
+			conn = DriverManager.getConnection("jdbc:sqlite:participants.sqlite");
+			return conn;
+			}else
+				return null;
+			}catch (ClassNotFoundException e) {
+				return null;
+				
+				} catch (SQLException e) {
+				return null;		
+			}
+
+	}
 }
