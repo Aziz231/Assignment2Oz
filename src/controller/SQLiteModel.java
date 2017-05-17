5c9fc0f7509f0a1864d5ebe855ce53892df69b74
turki <s3512039@student.rmit.edu.au>
2017-05-17 15:20:12 +1000

    database

--- src/controller/SQLiteModel.java
+++ src/controller/SQLiteModel.java
@@ -1,5 +1,24 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class SQLiteModel {
+	Connection conn;
+	 public SQLiteModel(){
+		 conn = SQLiteConnection.connect();
+	 }
+	 
+public boolean isDBConnected(){ 
+		 try {
+			return !conn.isClosed();
+		} catch (SQLException e) {
+			return false;
+		}		 
+	 }
+
+public Connection getConn() {
+	return conn;
+}
 
 }
